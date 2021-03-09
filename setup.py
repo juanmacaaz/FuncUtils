1a5555e5d9b74d08cf16000d4a8b74660270118d
Juan Manuel Camara <juanma.caaz@gmail.com>
2021-03-09 23:32:17 +0100

    Test

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='j-funcutils',  
-     version='0.1',
+     version='0.2',
      scripts=['funcutils'] ,
      author="Juan Manuel Camara Diaz",
      author_email="juanma.caaz@gmail.com",
